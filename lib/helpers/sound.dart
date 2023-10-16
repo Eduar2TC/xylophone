4fd27cffbb6d80daf61677aded2826febf0ac897
eduardoTC <edua2tc@gmail.com>
2023-10-16 06:13:58 -0600

    updt audioplayers package

--- lib/helpers/sound.dart
+++ lib/helpers/sound.dart
@@ -1,8 +1,12 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class Sound {
-  static void playSound(int option) {
-    AudioCache player = AudioCache(prefix: 'lib/assets/audio/');
-    player.play('note' + option.toString() + '.wav');
+  static void playSound(int option) async {
+    //AudioCache player = AudioCache(prefix: 'lib/assets/audio/');
+    //await player.play('note' + option.toString() + '.wav');
+    AudioPlayer player = AudioPlayer();
+    player.audioCache.prefix = 'lib/assets/audio/';
+    player.setPlayerMode(PlayerMode.mediaPlayer);
+    await player.play(AssetSource('note$option.wav'));
   }
 }
